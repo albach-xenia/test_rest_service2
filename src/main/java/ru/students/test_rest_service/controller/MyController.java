d36c53adfa593a4a3992618d46de5088d16130dd
albach-xenia <grigoryeva11@mail.ru>
2023-08-09 10:20:54 +0500

    add lr 2

--- src/main/java/ru/students/test_rest_service/controller/MyController.java
+++ src/main/java/ru/students/test_rest_service/controller/MyController.java
@@ -1,5 +1,8 @@
 package ru.students.test_rest_service.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -8,13 +11,22 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import ru.students.test_rest_service.model.Request;
 import ru.students.test_rest_service.model.Response;
+import ru.students.test_rest_service.service.MyModifyService;
 
+@Slf4j
 @RestController
 public class MyController {
+
+    private final MyModifyService myModifyService;
+    @Autowired
+    public MyController(@Qualifier("ModifyErrorMessage") MyModifyService myModifyService){
+        this.myModifyService = myModifyService;
+    }
 
     @PostMapping(value =  "/feedback")
     public ResponseEntity<Response> feedback(@RequestBody Request request){
 
+        log.info("Входящий request : " + String.valueOf(request));
         Response response = Response.builder()
                 .uid(request.getUid())
                 .operationUid(request.getOperationUid())
@@ -24,7 +36,8 @@ public class MyController {
                 .errorMessage("")
                 .build();
 
+        Response responseAfterModify = myModifyService.modify(response);
+        log.info("Исходящий response : " + String.valueOf(response));
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
-
 }
